bb3eab4753e8e4d8053824ea4aaf99b182a77b8d
cisco <cisco@UPOAULA10311>
2024-07-03 10:40:08 -0500

    Agregar archivos de proyecto.

---
+++ People/NCModels/NCPerson.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace People.NCModels
+{
+    [Table("people")]
+    internal class NCPerson
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Name { get; set; }
+    }
+}
